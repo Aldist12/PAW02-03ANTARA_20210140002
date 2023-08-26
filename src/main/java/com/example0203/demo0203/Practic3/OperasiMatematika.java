d4367e85a49cbb4b9686b4efa9f59711215a4cde
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2023-08-26 10:25:30 +0700

    add command

--- src/main/java/com/example0203/demo0203/Practic3/OperasiMatematika.java
+++ src/main/java/com/example0203/demo0203/Practic3/OperasiMatematika.java
@@ -6,22 +6,22 @@ public class OperasiMatematika {
         int a = 100;
         int b = 10;
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
+        System.out.println(a + b); // Penjumlahan: 100 + 10 = 110
+        System.out.println(a - b); // Pengurangan: 100 - 10 = 90
+        System.out.println(a * b); // Perkalian: 100 * 10 = 1000
+        System.out.println(a / b); // Pembagian: 100 / 10 = 10
+        System.out.println(a % b); // Modulus (Sisa pembagian): 100 % 10 = 0
 
-        // Augmented
+        // Augmented assignment (Penugasan dengan operasi)
         int c = 100;
 
-        c += 10;
+        c += 10; // c = c + 10; => 100 + 10 = 110
         System.out.println(c);
 
-        c -= 10;
+        c -= 10; // c = c - 10; => 110 - 10 = 100
         System.out.println(c);
 
-        c *= 10;
+        c *= 10; // c = c * 10; => 100 * 10 = 1000
         System.out.println(c);
     }
 }
